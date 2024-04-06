6b5425e86b490a97daecf55c6bdbc1aab56d10b0
tomaskone123 <tkkrumlovska@seznam.cz>
2024-04-06 19:20:23 +0200

    finished memset

--- sourcefiles/ft_memset.c
+++ sourcefiles/ft_memset.c
@@ -6,14 +6,31 @@
 /*   By: tomas <tomas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 19:19:34 by tomas             #+#    #+#             */
-/*   Updated: 2024/04/03 19:21:00 by tomas            ###   ########.fr       */
+/*   Updated: 2024/04/06 19:19:29 by tomas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <string.h>
 #include "libft.h"
 
-void *memset(void *s, int c, size_t n)
+void *ft_memset(void *s, int c, size_t n)
 {
-	
+	unsigned char* 	ptr;
+	size_t			i;
+
+	ptr = (unsigned char *)s;
+	while (i < n)
+	{
+		ptr[i] = (unsigned char)c;
+		i++;
+	}
+	return (s);
+}
+
+int main(void)
+{
+	char str[] = "Hello, World!";
+	ft_memset(str, 'x', 2);
+	printf("%s\n", str);
 }
--- sourcefiles/libft.h
+++ sourcefiles/libft.h
@@ -10,7 +10,7 @@ int	ft_isprint(int c);
 int ft_tolower(int c);
 int ft_toupper(int c);
 char *ft_strchr(const char *s, int c);
-void *memset(void *s, int c, size_t n);
+void *ft_memset(void *s, int c, size_t n);
 size_t ft_strlen(const char *s);
 
 #endif
