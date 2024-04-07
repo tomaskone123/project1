c551c29071fb8bb272050fc8c0d4c000c719bbf4
tomaskone123 <tkkrumlovska@seznam.cz>
2024-04-07 12:14:34 +0200

    created bzero function

--- sourcefiles/ft_bzero.c
+++ sourcefiles/ft_bzero.c
@@ -1,29 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tomas <tomas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/03 17:55:46 by tomas             #+#    #+#             */
-/*   Updated: 2024/04/07 12:10:43 by tomas            ###   ########.fr       */
+/*   Created: 2024/04/07 12:13:27 by tomas             #+#    #+#             */
+/*   Updated: 2024/04/07 12:13:28 by tomas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
-#include <stdio.h>
-#include <ctype.h>
-#include <string.h>
-
-int	main(void)
-{
-	char	c[50] = "hello world";
-	char	d = 'a';
-
-	bzero(c + 3, 1);
-	// memset(c, 'd', 8*sizeof(char));
-	// printf("%c\n", ft_tolower(d));
-	// printf("%s\n", ft_strchr(c, d));
-	printf("%s\n", c);
-	return 0;
-}
--- sourcefiles/libft.h
+++ sourcefiles/libft.h
@@ -12,5 +12,6 @@ int ft_toupper(int c);
 char *ft_strchr(const char *s, int c);
 void *ft_memset(void *s, int c, size_t n);
 size_t ft_strlen(const char *s);
+void ft_bzero(void *s, size_t n);q
 
 #endif
