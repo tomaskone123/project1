16db99e941bd5146480cc5977273b96b8b78753c
tomaskone123 <tkkrumlovska@seznam.cz>
2024-04-11 12:43:42 +0200

    finished strjoin.c

--- sourcefiles/ft_strjoin.c
+++ sourcefiles/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: tomas <tomas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 12:15:53 by tomas             #+#    #+#             */
-/*   Updated: 2024/04/11 12:24:45 by tomas            ###   ########.fr       */
+/*   Updated: 2024/04/11 12:43:21 by tomas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,27 @@
 char *ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	total;
+	char	*temp;
 	char	*new;
 
-	total = ft_strlen(s1) + ft_strlen(s2);
-	new = (char *)malloc(total * sizeof(char));
-	if (new == NULL)
+	total = strlen(s1) + strlen(s2); // prepsat strlen
+	temp = (char *)malloc(total * sizeof(char));
+	if (temp == NULL)
 		return NULL;
 	while (*s1)
-		s1++;
-
-
+		*(temp++) = *(s1++);
+	while (*s2)
+		*(temp++) = *(s2++);
+	temp -= total;
+	return (temp);
 }
+
+// int main(void)
+// {
+// 	const char *a = "hello ";
+// 	const char *b = "world";
+// 	char *n;
+
+// 	n = ft_strjoin(a, b);
+// 	printf("%s\n", n);
+// }
