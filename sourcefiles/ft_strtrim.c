b8708d3d9d6fe85e2de98e30a1444545fd629b83
tomaskone123 <tkkrumlovska@seznam.cz>
2024-04-14 13:36:23 +0200

    nevim

--- sourcefiles/ft_strtrim.c
+++ sourcefiles/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: tomas <tomas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 13:31:14 by tomas             #+#    #+#             */
-/*   Updated: 2024/04/12 18:25:37 by tomas            ###   ########.fr       */
+/*   Updated: 2024/04/14 13:25:45 by tomas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,15 @@ char *ft_strtrim(char const *s1, char const *set)
 {
 	char	*temp;
 	size_t	i;
-	size_t	total;
-	char	*setstart;
 
-	setstart = set;
 	i = 0;
-	total = ft_strlen(s1) - 1;
-	temp = (char *)malloc(total * sizeof(char)); //predelat strlen na ft_strlen
+	temp = (char *)malloc((ft_strlen(s1) + 1) * sizeof(char)); //predelat strlen na ft_strlen
 	if (temp == NULL)
 		return (NULL);
-	while (s1[i] != '\0')
+	while ()
 	{
-		while (s1[i] == *set)
-		{
-			i++;
-			set++;
-		}
-		set = setstart;
-		temp[i] = s1[i];
-		i++;
+
 	}
-	// temp -= total;
-	return (temp);
+
 }
 
