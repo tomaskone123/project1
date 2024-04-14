8f46e1f749127e8b81486798f7b1b9b16fd237b0
tomaskone123 <tkkrumlovska@seznam.cz>
2024-04-14 12:53:53 +0200

    bababab

--- sourcefiles/ft_strtrim.c
+++ sourcefiles/ft_strtrim.c
@@ -6,18 +6,18 @@
 /*   By: tomas <tomas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 13:31:14 by tomas             #+#    #+#             */
-/*   Updated: 2024/04/11 13:37:50 by tomas            ###   ########.fr       */
+/*   Updated: 2024/04/12 18:28:18 by tomas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
-// #include <unistd.h>
-// #include <stdlib.h>
-// #include <stdio.h>
+#include "libft.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-// char *ft_strtrim(char const *s1, char const *set)
-// {
-// 	const char *temp;
+char *ft_strtrim(char const *s1, char const *set)
+{
+	const char *temp;
 
-// 	temp = (char *)malloc((strlen(s1) - 1) * sizeof(char));
-// }
+	temp = (char *)malloc((strlen(s1) - 1) * sizeof(char));
+}
