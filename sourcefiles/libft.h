bf50d2346fc0278f0f615511ea12244adda78d3c
tomaskone123 <tkkrumlovska@seznam.cz>
2024-04-24 20:46:16 +0200

    started working on itoa

--- sourcefiles/libft.h
+++ sourcefiles/libft.h
@@ -27,5 +27,6 @@ void *ft_calloc(size_t nmemb, size_t size);
 char *ft_strjoin(char const *s1, char const *s2);
 char *ft_strtrim(char const *s1, char const *set);
 char *ft_strcpy(char *dest, const char *src);
+char *ft_itoa(int n);
 
 #endif
